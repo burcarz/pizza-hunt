edcc842c1af16561f9f30e6c9af88f5ac22ec02b
burcar <zacharyburcar@gmail.com>
2022-02-06 21:10:18 -0500

    added validators

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -5,6 +5,12 @@ const { addComment, removeComment, addReply, removeReply } = require('../../cont
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
+
+// /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
 
 module.exports = router;
